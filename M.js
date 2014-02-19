a2bf3d3c296c45909dba0fad1bf239a0ce11c720
githubyang <281627989@qq.com>
2014-02-19 11:33:43 +0800

    重构框架结构

--- M.js
+++ M.js
@@ -1,42 +1,60 @@
-/* ==================================================
- * 依赖jquery核心的js模块化加载框架M
- * 版本:v 1.0.1
- *==================================================*/
 (function (win, doc, undefined) {
     'use strict';
+    // 框架常用的变量声明
     var loaded = {},// 已加载模块
         loadList = {},// 已加载列表
         loadingFiles = {},// 加载中的模块
+                
+        globalList = [],// 全局模块
+        readyList = [],
+        isReady = false,
         // 内部配置文件
         config = {
             autoLoad : true,// 是否自动加载核心库
             timeout : 100,// 加载延迟
-            coreLib : ['/LQY/js/jquery.1.6.1.js'],// 核心库
+            coreLib : ['/M/js/jquery.Small.min.1.0.js'],// 核心库
             model: {}
         },
-        jsSelf = (function (a) {
-          var files = a.getElementsByTagName('script');
-          return files[files.length - 1];
-        }(doc)),
-        globalList = [],// 全局模块
-        readyList = [],// domready回调堆栈
-        isReady = false,// DOM Ready 
-        _isArray = function (e) {
+        uid=1,//用来异步执行的变量
+        map={},//存放异步执行的字面量
+        rmap={},//存放异步执行的字面量
+        jsReference=[];// 加载外部模块参考位置
+    // 初始外部配置
+    (function(){
+        var jsObj = (function (a) {
+            var files = a.getElementsByTagName('script');
+            jsReference[0] = files[files.length - 1];
+            return files[files.length - 1];
+        }(doc));
+        (function(a){
+            var obj=a,
+                initAuto=obj.getAttribute('auto'),//是否关闭自动加载
+                initCore=obj.getAttribute('core');//外部加载的核心库
+            if(initAuto){
+                config.autoLoad=(initAuto.toLowerCase()==='false')?false:true;
+            }
+            if(initCore){
+                config.coreLib=initCore.split(',');
+            }
+        }(jsObj||{}));
+    }());
+    // 内部方法
+    var method={
+        _isArray : function (e) {
             return e.constructor === Array;
         },
-        // @deps[i++]作为参数传入
-        _getMod = function (e){
+        _getMod : function (e){
             var model = config.model,
                 mod; 
             if(typeof e === 'string') {
-               mod=(model[e]) ? model[e] : {path : e};
+                mod=(model[e]) ? model[e] : {path : e};
             }else{
-               mod = e;
+                mod = e;
             }
             return mod;
         },
-        _load = function (url, type, charset, cb) {
-            var wait,n,t,img, 
+        _load : function (url, type, charset, cb) {
+            var wait,nodeStr,t,img, 
             done = function () {
                 loaded[url] = 1;
                 cb && cb(url);
@@ -53,7 +71,7 @@
             }
             if(loadingFiles[url]){
                 setTimeout(function(){
-                    _load(url,type,charset,cb);
+                    method._load(url,type,charset,cb);
                 },10);
                 return;
             }
@@ -65,47 +83,35 @@
             },config.timeout);
             t=type || url.toLowerCase().split(/\./).pop().replace(/[\?#].*/,'');
             if(t==='js'){
-                n=doc.createElement('script');
-                n.setAttribute('type','text/javascript');
-                n.setAttribute('src',url);
-                n.setAttribute('async',true);
+                nodeStr=doc.createElement('script');
+                nodeStr.setAttribute('type','text/javascript');
+                nodeStr.setAttribute('src',url);
+                nodeStr.setAttribute('async',true);
             }else if(t==='css'){
-                n=doc.createElement('link');
-                n.setAttribute('type', 'text/css');
-                n.setAttribute('rel', 'stylesheet');
-                n.setAttribute('href', url);
+                nodeStr=doc.createElement('link');
+                nodeStr.setAttribute('type', 'text/css');
+                nodeStr.setAttribute('rel', 'stylesheet');
+                nodeStr.setAttribute('href', url);
             }
-            if(charset){n.charset=charset;}
-            if(t==='img'){
-                img=new Image();
-                img.onerror=function(){
+            if(charset){nodeStr.charset=charset;}
+            nodeStr.onerror=function(){done();nodeStr.onerror=null;};
+            nodeStr.onload=nodeStr.onreadystatechange=function(){
+                var url;
+                if(!this.readyState || this.readyState === 'loaded' || this.readyState === 'complete'){
                     done();
-                    img.onerror=null;
-                    img=null;
+                    nodeStr.onload=nodeStr.onreadystatechange=null;
                 }
-                img.src=url;
-            }else{
-                n.onerror=function(){done();n.onerror=null;};
-                n.onload=n.onreadystatechange=function(){
-                    var url;
-                    if(!this.readyState || this.readyState === 'loaded' || this.readyState === 'complete'){
-                        done();
-                        n.onload=n.onreadystatechange=null;
-                    }
-                };
-            }
-            jsSelf.parentNode.insertBefore(n,jsSelf);
+            };
+            jsReference[0].parentNode.insertBefore(nodeStr,jsReference[0]);
         },
-        // 加载依赖文件
-        _loadDeps=function(deps,cb){
+        // 加载依赖论文件(顺序)
+        _loadDeps:function(deps,cb){
             var model=config.model,
-                id,
+                id=deps.join(''),
                 m,
                 mod,
                 i=0,
-                len;
-            id=deps.join('');
-            len=deps.length;
+                len=deps.length;
             if(loadList[id]){
                 cb();
                 return;
@@ -118,19 +124,19 @@
                 }
             }
             for(;m=deps[i++];){
-                mod=_getMod(m);
+                mod=method._getMod(m);
                 if(mod.requires){
-                    _loadDeps(mod.requires,(function(m){
+                    method._loadDeps(mod.requires,(function(m){
                         return function(){
-                            _load(m.path,m.type,m.charset,callback);
+                            method._load(m.path,m.type,m.charset,callback);
                         };
                     }(mod)));
                 }else{
-                    _load(mod.path,mod.type,mod.charset,callback);
+                    method._load(mod.path,mod.type,mod.charset,callback);
                 }
             }
         },
-        contentLoaded=function(fn){
+        contentLoaded:function(fn){
             var done=false,top=true, 
                 doc=win.document, 
                 root=doc.documentElement,
@@ -138,16 +144,17 @@
                 rem=doc.addEventListener ? 'removeEventListener' : 'detachEvent',
                 pre=doc.addEventListener ? '' : 'on',
                 init=function(e){
-                    if(e.type == 'readystatechange' && doc.readyState != 'complete') return;
+                    if(e.type == 'readystatechange' && doc.readyState != 'complete'){return;}
                     (e.type == 'load' ? win : doc)[rem](pre + e.type, init, false);
-                    if (!done && (done = true)) fn.call(win, e.type || e);
+                    if (!done && (done = true)){fn.call(win, e.type || e);}
                 },
                 poll=function(){
                     try {root.doScroll('left');} catch(e){setTimeout(poll, 50); return;}
                     init('poll');
                 };
-            if(doc.readyState == 'complete') fn.call(win, 'lazy');
-            else {
+            if(doc.readyState == 'complete'){
+                fn.call(win, 'lazy');
+            }else{
                 if (doc.createEventObject && root.doScroll) {
                     try { top = !win.frameElement; } catch(e) { }
                     if (top) {
@@ -159,7 +166,7 @@
                 win[add](pre + 'load', init, false);
             }
         },
-        fireReadyList=function(){
+        fireReadyList:function(){
             var i=0,list;
             if (readyList.length){
                 for(;list=readyList[i++];){
@@ -167,32 +174,32 @@
                 }
             }
         },
-        uid=1,//用来异步执行的变量
-        map={},//存放异步执行的字面量
-        rmap={},//存放异步执行的字面量
-        indexOf=Array.prototype.indexOf || function(obj){
-            for(var i=0,len=this.length;i<len;++i){
-                if(this[i]===obj){return i;}
-            }
-            return -1;
-        },
-        fire=function(callback,thisObj){
+        //某些方面讲这是个异步执行的核心方法 这方法还有优化空间
+        fire:function(callback,thisObj){
             setTimeout(function(){
                 callback.call(thisObj);
             },0);
-        };//某些方面讲这是个异步执行的核心方法 这方法还有优化空间
+        }
+    },
+    // 查找数组里面当前元素的位置
+    indexOf=Array.prototype.indexOf || function(obj){
+        for(var i=0,len=this.length;i<len;++i){
+            if(this[i]===obj){return i;}
+        }
+        return -1;
+    };
     var M=function(){
         var args=[].slice.call(arguments),fn,id;
         // 加载核心库
         if(config.autoLoad && !loadList[config.coreLib.join('')]){
-            _loadDeps(config.coreLib,function(){
+            method._loadDeps(config.coreLib,function(){
                 M.apply(null,args);
             });
             return;
         }
         // 加载全局库
         if(globalList.length > 0 && !loadList[globalList.join('')]){
-            _loadDeps(globalList,function(){
+            method._loadDeps(globalList,function(){
                 M.apply(null,args);
             });
             return;
@@ -206,11 +213,13 @@
             return;
         }
         //正常加载
-        _loadDeps(args,function(){
+        method._loadDeps(args,function(){
             loadList[id] = 1;
             fn && fn();
         });
+        return M;
     };
+    //异步执行的内置方法
     M._release=function(res,list){
         var maps=map,
             rmaps=rmap;
@@ -221,19 +230,21 @@
                 pos=indexOf.call(waiting,res);
                 waiting.splice(pos,1);
             if (waiting.length===0){
-                fire(mapItem.callback,mapItem.thisObj);
+                method.fire(mapItem.callback,mapItem.thisObj);
                 delete maps[uid];
             }
         }
-    };//异步执行的内置方法
+    };
     //============================= 静态方法 ===========================
     M.add=function(sName,oConfig){
         if(!sName || !oConfig || !oConfig.path){return;}
         config.model[sName]=oConfig;
+        return this;
     };
     M.global=function(){
-        var args=_isArray(arguments[0])?arguments[0]:[].slice.call(arguments);
+        var args=method._isArray(arguments[0])?arguments[0]:Array.prototype.slice.call(arguments);
         globalList=globalList.concat(args);
+        return this;
     };
     M.css=function(s){
         var css=doc.getElementById('do-inline-css');
@@ -241,17 +252,18 @@
             css=doc.createElement('style');
             css.type='text/css';
             css.id='do-inline-css';
-            jsSelf.parentNode.insertBefore(css,jsSelf);
+            jsReference[0].parentNode.insertBefore(css,jsReference[0]);
         }
         if(css.styleSheet){
             css.styleSheet.cssText=css.styleSheet.cssText + s;
         }else{
             css.appendChild(doc.createTextNode(s));
         }
+        return this;
     };
     M.SET=function(n,v){
         config[n]=v;
-        return M;
+        return this;
     };
     M.GET=function(n){
         return config[n];
@@ -280,6 +292,53 @@
         }
         return Child;
     };
+    // 异步执行外部调用的等待方法
+    M.when=function(resources, callback, thisObj){
+        var maps=map,
+            rmaps=rmap;
+        if(typeof resources === 'string'){resources=[resources];}
+        var id=(uid++).toString(16);
+        maps[id]={
+            waiting:resources.slice(0),
+            callback:callback,
+            thisObj:thisObj || window
+        };
+        for (var i=0,len=resources.length;i<len;++i){
+            var res=resources[i],
+                list=rmaps[res] || (rmaps[res]=[]);
+            list.push(id);
+        }
+        return this;
+    };
+    // 异步执行外部调用的触发方法
+    M.trigger=function(resources){
+        if(!resources){return this;}
+        var maps=map,
+            rmaps=rmap;
+        if(typeof resources==='string'){resources=[resources];}
+        for(var i=0,len=resources.length;i<len;++i){
+            var res=resources[i];
+            if (typeof rmaps[res]==='undefined') continue;
+            M._release(res,rmaps[res]);
+            delete rmaps[res];
+        }
+        return this;
+    };
+    //延迟执行
+    M.delay=function(){
+        var args=[].slice.call(arguments,0),
+            delay=args.shift();
+        setTimeout(function(){
+            M.apply(this,args);
+        },delay);
+        return this;
+    };
+    method.contentLoaded(function(){
+        isReady=true;
+        method.fireReadyList();
+    });
+    win.$$=win.M=M;
+    
     //基于原型链的继承 框架最早实现的继承方式
     Function.prototype.extend=function(a,b){
         //保证传进来的参数是函数
@@ -303,59 +362,4 @@
         this.prototype.constructor=this;//保证指向继承函数自身
         return this;
     };
-    M.when=function(resources, callback, thisObj){
-        var maps=map,
-            rmaps=rmap;
-        if(typeof resources === 'string'){resources=[resources];}
-        var id=(uid++).toString(16);
-        maps[id]={
-            waiting:resources.slice(0),
-            callback:callback,
-            thisObj:thisObj || window
-        };
-        for (var i=0,len=resources.length;i<len;++i){
-            var res=resources[i],
-                list=rmaps[res] || (rmaps[res]=[]);
-            list.push(id);
-        }
-        return this;
-    };//异步执行外部调用的等待方法
-    M.trigger=function(resources){
-        if(!resources){return this;}
-        var maps=map,
-            rmaps=rmap;
-        if(typeof resources==='string'){resources=[resources];}
-        for(var i=0,len=resources.length;i<len;++i){
-            var res=resources[i];
-            if (typeof rmaps[res]==='undefined') continue;
-            M._release(res,rmaps[res]);
-            delete rmaps[res];
-        }
-        return this;
-    };//异步执行外部调用的触发方法
-    M.delay=function(){
-        var args=[].slice.call(arguments,0),
-            delay=args.shift();
-        setTimeout(function(){
-            M.apply(this,args);
-        },delay);
-    };//延迟执行
-    contentLoaded(function(){
-        isReady=true;
-        fireReadyList();
-    });
-    // 初始外部配置
-    (function(a){
-        var initAuto,
-            initCore;
-        initAuto=a.getAttribute('auto');//是否关闭自动加载
-        if(initAuto){
-            config.autoLoad=(initAuto.toLowerCase()==='false')?false:true;
-        }
-        initCore=a.getAttribute('core');//外部加载的核心库
-        if(initCore){
-            config.coreLib=initCore.split(',');
-        }
-    }(jsSelf||{}));
-    win.$$=M;
 }(window,document));
